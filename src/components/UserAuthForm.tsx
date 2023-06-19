255b95f2bffbed40277da869cd96071b52db2d9f
vitheshshetty00 <vitheshshetty23@gmail.com>
2023-06-19 22:51:28 +0530

    ..

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,13 +1,37 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
 import { Button } from "./ui/Button";
 import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { Icon } from "./Icons";
 
-interface UserAuthFormProps {}
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-const UserAuthForm: FC<UserAuthFormProps> = ({}) => {
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	const loginWithGoogle = async () => {
+		setIsLoading(true);
+		try {
+			await signIn("google");
+		} catch (error) {
+			//toast notification
+		} finally {
+			setIsLoading(false);
+		}
+	};
 	return (
-		<div className={cn('flex justify-center')}>
-			<Button>Google</Button>
+		<div className={cn("flex justify-center", className)} {...props}>
+			<Button
+				onClick={loginWithGoogle}
+				isLoading={isLoading}
+				size="sm"
+				className="w-full"
+			>
+				{isLoading ? null : <Icon.google className="h-4 w-4 mr-2" />}
+				Google
+			</Button>
 		</div>
 	);
 };
